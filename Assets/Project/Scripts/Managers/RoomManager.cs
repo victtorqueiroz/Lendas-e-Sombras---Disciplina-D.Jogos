a68a1a225142e80347a6f77592e0faee839efdd8
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-06-23 10:17:27 -0300

    feat: RoomManager

--- Assets/Project/Scripts/Managers/RoomManager.cs
+++ Assets/Project/Scripts/Managers/RoomManager.cs
@@ -1,16 +1,58 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Refs")]
+    [SerializeField] private EnemyFactory enemyFactory;
+    [SerializeField] private RoomExit exitTrigger;
+    
+    [Header("Spawns")]
+    [SerializeField] private List<Transform> spawnPoints;
+    
+    private int aliveEnemiesCount = 0;
+
+    private void Start()
     {
-        
+        StartRoom();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartRoom()
     {
+        // teste rapido: spawna 1 corredor no primeiro ponto
+        if (spawnPoints.Count > 0)
+        {
+            SpawnEnemy(EnemyType.Corredor, spawnPoints[0]);
+        }
         
+        CheckRoomClear();
+    }
+
+    private void SpawnEnemy(EnemyType type, Transform spawnPoint)
+    {
+        Enemy newEnemy = enemyFactory.CreateEnemy(type, spawnPoint.position, spawnPoint.rotation);
+        
+        if (newEnemy != null)
+        {
+            newEnemy.OnDied += HandleEnemyDeath; // escuta a morte do bicho
+            aliveEnemiesCount++;
+        }
+    }
+
+    private void HandleEnemyDeath(Enemy deadEnemy)
+    {
+        deadEnemy.OnDied -= HandleEnemyDeath; // limpa a escuta pra n vazar memoria
+        aliveEnemiesCount--;
+        
+        CheckRoomClear();
+    }
+
+    private void CheckRoomClear()
+    {
+        // matou todos = libera a porta
+        if (aliveEnemiesCount <= 0)
+        {
+            exitTrigger.Unlock();
+        }
     }
 }
