fd1aa091fb6d1477f9d11dc58948497f8a126ff0
ClidenorFilho <clidenorlopesmartinsfilho@gmail.com>
2026-06-23 10:06:48 -0300

    feat: classe CorredorEnemy

---
+++ Assets/Project/Scripts/Enemies/CorredorEnemy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CorredorEnemy : Enemy
+{
+    protected override void Start()
+    {
+        base.Start(); // chama o start do molde base
+        
+        // ajusta a vida pro corredor (mais fraco)
+        maxHealth = 50f;
+        currentHealth = maxHealth;
+    }
+}
